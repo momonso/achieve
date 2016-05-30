44e3ef8204a733b2c5a3dd0895d65a0c07352c97
Nakamura <te105164@yahoo.co.jp>
2016-05-30 08:52:52 +0000

    DAY05 login実装 ログイン制限前

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  
+  
+  config.action_mailer.default_url_options = { host: 'sleepy-crag-89337.herokuapp.com' }
+  
+  
 end
