70554b65e262fd7ea525aafce4bbafed2320f14b
nakamura <te105164@yahoo.co.jp>
2016-05-31 09:25:42 +0000

    課題5 注意文の日本語化

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Achieve
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.i18n.default_locale = :ja
   end
 end
